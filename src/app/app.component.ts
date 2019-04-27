1c392ff89da49bc94c2ab60f03272218e75d4214
Radosław Białowąs <radoslaw.bialowas@gmail.com>
2019-04-27 21:55:01 +0200

    feat(Configured P5 integration with Angular):

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import * as P5 from 'p5';
+
+const sketch = (p: P5) => {
+
+  p.preload = () => {
+
+  };
+
+  p.setup = () => {
+    p.createCanvas(p.windowWidth, p.windowHeight);
+  };
+
+  p.windowResized = () => {
+    p.resizeCanvas(p.windowWidth, p.windowHeight);
+  };
+
+  p.draw = () => {
+    p.background(255);
+    p.text('P5 working fine.', 100, 100);
+  };
+};
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-p5';
+export class AppComponent implements OnInit {
+  p5: P5;
+  @ViewChild('sketch') sketch: ElementRef;
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    this.p5 = new P5(sketch, this.sketch.nativeElement);
+  }
+
 }
